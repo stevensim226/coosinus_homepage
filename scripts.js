63843101d92345e3296ba58e143d7dc0b0598985
Steven Sim <stevensim226@gmail.com>
2020-04-12 20:34:12 +0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,29 @@
+$(function() {
+    var tabButtons = $(".tab-button")
+    var tablePictures = $(".table-mobile")
+
+    tabButtons.click((e) => {
+        var foundText = $(e.target).find("h5").text()
+        var target = e.target
+        if (foundText == "") {
+            foundText = $(e.target.parentElement).find("h5").text()
+            target = e.target.parentElement
+        }
+        
+        $(target).addClass("selected-table-tab-mobile")
+        switch (foundText) {
+            case "Bimbel Jadul":
+                $(tabButtons[1]).removeClass("selected-table-tab-mobile")
+                $(tablePictures[1]).removeClass("selected-table")
+                $(tablePictures[1]).addClass("not-selected-table")
+                $(tablePictures[0]).removeClass("not-selected-table")
+                break
+            case "Coosinus":
+                $(tabButtons[0]).removeClass("selected-table-tab-mobile")
+                $(tablePictures[0]).removeClass("selected-table")
+                $(tablePictures[0]).addClass("not-selected-table")
+                $(tablePictures[1]).removeClass("not-selected-table")
+                break
+        }
+    })
+})
